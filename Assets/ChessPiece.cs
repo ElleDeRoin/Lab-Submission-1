617ae217756b9f401257557e7b904d45a7a6ecee
ElleDeRoin <107569262+ElleDeRoin@users.noreply.github.com>
2026-09-02 19:49:16 -0400

    aaaaah

--- Assets/ChessPiece.cs
+++ Assets/ChessPiece.cs
@@ -5,41 +5,63 @@ using UnityEditor;
 
 public class ChessPiece : MonoBehaviour
 {
-    public static string[] options = new string[] { "Pawn", "Rook", "Knight", "Bishop", "Queen", "King" };
-    private static int index = 0;
-    [MenuItem("UI Toolkit/Example")]
-    
-    public static void ShowPopup()
+    public enum options { Pawn, Rook, Knight, Bishop, Queen, King };
+    public options currentOption = options.Pawn;
+
+
+
+    private void OnDrawGizmos()
     {
-        index = EditorGUILayout.Popup(index, options);
+        switch (currentOption)
+        {
+            case options.Pawn:
+                Gizmos.DrawIcon(transform.position, "Chess_pawn.png");
+                break;
+            case options.Rook:
+                Gizmos.DrawIcon(transform.position, "Chess_rook.png");
+                break;
+            case options.Knight:
+                Gizmos.DrawIcon(transform.position, "Chess_knight.png");
+                break;
+            case options.Bishop:
+                Gizmos.DrawIcon(transform.position, "Chess_bishop.png");
+                break;
+            case options.Queen:
+                Gizmos.DrawIcon(transform.position, "Chess_queen.png");
+                break;
+            case options.King:
+                Gizmos.DrawIcon(transform.position, "Chess_king.png");
+                break;
+        }
+
     }
 
-
-
-    public void ChangePieceType(string type)
+    private void OnDrawGizmosSelected()
     {
-        switch (type) {
-            case "Chess_pawn":
-            // Change the piece to a pawn
-            break;
-        case "Chess_rook":
-            // Change the piece to a rook
-            break;
-        case "Chess_knight":
-            // Change the piece to a knight
-            break;
-        case "Chess_bishop":
-            // Change the piece to a bishop
-            break;
-        case "Chess_queen":
-            // Change the piece to a queen
-            break;
-        case "Chess_king":
-            // Change the piece to a kings
-            break;
+        Gizmos.color = Color.yellow;
+
+        switch (currentOption)
+        {
+            case options.Pawn:
+                Gizmos.DrawWireSphere(transform.position, 0.5f);
+                break;
+            case options.Rook:
+                Gizmos.DrawWireCube(transform.position, new Vector3(1, 1, 1));
+                break;
+            case options.Knight:
+                Gizmos.DrawWireSphere(transform.position, 0.5f);
+                break;
+            case options.Bishop:
+                Gizmos.DrawWireSphere(transform.position, 0.5f);
+                break;
+            case options.Queen:
+                Gizmos.DrawWireSphere(transform.position, 0.5f);
+                break;
+            case options.King:
+                Gizmos.DrawWireSphere(transform.position, 0.5f);
+                break;
         }
-        return;
+
+
     }
 }
-
-
