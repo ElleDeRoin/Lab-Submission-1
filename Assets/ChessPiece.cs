e2c487c52051e02e67c61cccdf2822ed8f2e83ae
Cami Alarcon-Fernandez <camialarconfern@gmail.com>
2026-09-02 18:06:53 -0400

    Added sprites and created ChessPiece.cs

---
+++ Assets/ChessPiece.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessPiece : MonoBehaviour
+{
+    void ChangePieceType(string type)
+    {
+        switch (type)
+            case "Chess_pawn":
+            // Change the piece to a pawn
+                break;
+            case "Chess_rook":
+            // Change the piece to a rook
+                break;
+            case "Chess_knight":
+            // Change the piece to a knight
+                break;
+            case "Chess_bishop":
+            // Change the piece to a bishop
+                break;
+            case "Chess_queen":
+            // Change the piece to a queen
+                break;
+            case "Chess_king":
+            // Change the piece to a king
+                break;
+        }
+
+    }
