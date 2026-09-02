6978b89caf06b97c768de54e513e49ef96cf0488
Cami Alarcon-Fernandez <camialarconfern@gmail.com>
2026-09-02 18:40:50 -0400

    Did some weird stuff! Idk! Yay!

--- Assets/ChessPiece.cs
+++ Assets/ChessPiece.cs
@@ -1,30 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class ChessPiece : MonoBehaviour
 {
-    void ChangePieceType(string type)
+    public static string[] options = new string[] { "Pawn", "Rook", "Knight", "Bishop", "Queen", "King" };
+    private static int index = 0;
+    [MenuItem("UI Toolkit/Example")]
+    
+    public static void ShowPopup()
     {
-        switch (type)
+        index = EditorGUILayout.Popup(index, options);
+    }
+
+
+
+    public void ChangePieceType(string type)
+    {
+        switch (type) {
             case "Chess_pawn":
             // Change the piece to a pawn
-                break;
-            case "Chess_rook":
+            break;
+        case "Chess_rook":
             // Change the piece to a rook
-                break;
-            case "Chess_knight":
+            break;
+        case "Chess_knight":
             // Change the piece to a knight
-                break;
-            case "Chess_bishop":
+            break;
+        case "Chess_bishop":
             // Change the piece to a bishop
-                break;
-            case "Chess_queen":
+            break;
+        case "Chess_queen":
             // Change the piece to a queen
-                break;
-            case "Chess_king":
-            // Change the piece to a king
-                break;
+            break;
+        case "Chess_king":
+            // Change the piece to a kings
+            break;
         }
-
+        return;
     }
+}
+
+
